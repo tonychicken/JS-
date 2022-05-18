4b38b07ab028521870e6882918670063c385d365
tonychicken <abcdqazzaq@gmail.com>
2022-05-18 21:17:55 +0800

    XHR_JSON_GET

--- w13/3_json_people-xhr/app.js
+++ w13/3_json_people-xhr/app.js
@@ -1,21 +1,31 @@
 const btn=document.querySelector('.btn');
 
+const url='./api/people.json';
+
 btn.addEventListener('click',()=>{
-    getData();
+    getData(url);
 });
 
-function getData(){
+function getData(url){
     const xhr =new XMLHttpRequest();
     console.log('xhr0',xhr); 
 
-    xhr.open('GET','./api/sample.txt');
+    xhr.open('GET',url);
     xhr.onreadystatechange=function(){
     console.log('xhr',xhr);
     if(xhr.readyState===4&& xhr.status===200){
         // console.log('done');
-        const text=document.createElement('p');
-        text.textContent=xhr.responseText;
-        document.body.appendChild(text);
+        const data=JSON.parse(xhr.responseText);
+        console.log('data',data);
+        const displayData=data
+        .map((item)=>{
+            return `<p>${item.name}</p>`
+        })
+        .join('');
+        console.log('displayData',displayData);
+        const element=document.createElement('div');
+        element.innerHTML=displayData;
+        document.body.appendChild(element);
     }else{
         console.log({
             status:xhr.status,
