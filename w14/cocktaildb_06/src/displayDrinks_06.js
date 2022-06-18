828eb1a64ad8dd46c36a8eeb6eefe83a8d90f371
tony.zhan <tony.zhan@shinda.com.tw>
2022-06-19 04:47:05 +0800

    Dinamic_HTML_From_API

--- w14/cocktaildb_06/src/displayDrinks_06.js
+++ w14/cocktaildb_06/src/displayDrinks_06.js
@@ -1,16 +1,30 @@
 import get from './getElement.js';
 
-const displayDrinks = async ({drinks}) =>{
-    console.log('drink',drinks);
+const displayDrinks = async ({ drinks }) => {
+    console.log('drink', drinks);
     const title = get('.title');
     const section = get('.section-center');
     // console.log(title);
 
-    if(!drinks){
+    if (!drinks) {
         title.textContent = 'Sorry,No drinks matched your search';
         section.innerHTML = null;
-    return;
+        return;
     }
+    const newDrinks = drinks.map((drink) => {
+        const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+        return `     
+            <a href="drink_06.html">
+            <article class="cocktail" data-id="${id}}">
+            <img src="${image}" alt="cocktail">
+            <h3>${name}</h3>
+            </article>
+            </a>
+            `
+    }).join('');//JOIN不要陣列，要字串
+    title.textContent = '';
+    section.innerHTML =newDrinks;//從API動態產生圖片
+
     return section;
 }
 
