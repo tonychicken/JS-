ffb459a677951045e71ac6c126ce412181b6d88a
tony.zhan <tony.zhan@shinda.com.tw>
2022-06-19 06:01:50 +0800

    localStorage_implementf

--- w15/cocktaildb_06/src/presentDrinks_06.js
+++ w15/cocktaildb_06/src/presentDrinks_06.js
@@ -1,10 +1,17 @@
 import fetchDrinks from './fetchDrinks_06.js';
 import displayDrinks from './displayDrinks_06.js';
+import setDrinks from './setDrink_06.js';
+
 
 const presentDrinks =async (url)=>{
     const data = await fetchDrinks(url);
     console.log('fetch data',data);
-    const section = await displayDrinks(data);
+    const section = await displayDrinks(data);//搜尋主頁面
+
+    if(section){
+        setDrinks(section);
+    }
+
 }
 
 export default presentDrinks;
--- w15/cocktaildb_06/src/setDrink_06.js
+++ w15/cocktaildb_06/src/setDrink_06.js
@@ -1,5 +1,10 @@
 const setDrinks =(section)=>{
-    
+    section.addEventListener('click',(e)=>{
+        e.preventDefault();
+        // console.log('e.target',e.target.parentElement.dataset.id);  //測試
+        const id=e.target.parentElement.dataset.id;
+        localStorage.setItem('drink',id);
+    })
 }
 
 export default setDrinks;
