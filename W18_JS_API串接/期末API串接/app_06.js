bf3832e3940fd8817c0b3d2e427c3b25df01dc9f
tony.zhan <tony.zhan@shinda.com.tw>
2022-06-22 09:54:46 +0800

    Update_ClassDemo_each_week

--- W18_JS_API串接/期末API串接/app_06.js
+++ W18_JS_API串接/期末API串接/app_06.js
@@ -7,6 +7,15 @@ import { LOCAL_URL,hasNetwork,serchItems } from "./api/search/config_06.js";
 
 const URL ='https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=25';
 
+    function mbar(sobj) {
+    var docurl =sobj.options[sobj.selectedIndex].value;
+    if (docurl != "") {
+    open(docurl,'_blank');
+    sobj.selectedIndex=0;
+    sobj.blur();
+    }
+    }
+
 window.addEventListener('DOMContentLoaded',()=>{
     if(hasNetwork){
         presentAnimals(URL);
--- W18_JS_API串接/期末API串接/src/searchForm_06.js
+++ W18_JS_API串接/期末API串接/src/searchForm_06.js
@@ -3,13 +3,17 @@ import presentAnimals from "./presentAnimals_06.js";
 
 const form =get('.search-form');
 
+const change=get('[name="select"]');
+
 const input = get('[name="Animal"]');
 
-const baseURL ='https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=25';
+const baseURL ='https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=';
 
 form.addEventListener('keyup',(e)=>{
     e.preventDefault();//取消預設方法
     console.log('input',input.value);//監聽查看
-    const value="&animal_kind="+input.value;
+    const value2=change.value;
+    console.log('dfdf',change.value);//監聽查看
+    const value=input.value;
     presentAnimals(`${baseURL}${value}`);//監聽input.value，搜尋API內物件
 });
